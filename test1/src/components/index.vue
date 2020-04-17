8e664f061850a7f128b71afecf764c026ce052a9
zq0719 <1617527089@qq.com>
2020-04-17 11:10:28 +0800

    first

---
+++ test1/src/components/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>
+      <input type="text" v-model="inputValue" />
+      <button @click="handleSubmit">提交</button>
+    </div>
+    <ul>
+      <li v-for="(item,index) of list" :key="index">{{item}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      inputValue: "",
+      list: []
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.list.push(this.inputValue);
+      this.inputValue = "";
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
--- test1/src/router/index.js
+++ test1/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import index from '../components/index.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'index',
+    component: index
   },
   {
     path: '/about',
